7c82655dbab54d450f943ce02b9db5f5c7675bc1
Mstone8370 <mstone8370@gmail.com>
2025-04-10 03:25:59 +0900

    minor fix

--- Engine/Engine/Source/Runtime/Launch/EngineLoop.cpp
+++ Engine/Engine/Source/Runtime/Launch/EngineLoop.cpp
@@ -360,7 +360,7 @@ void FEngineLoop::WindowInit(HINSTANCE hInstance)
 
     hWnd = CreateWindowExW(
         0, WindowClass, Title, WS_POPUP | WS_VISIBLE | WS_OVERLAPPEDWINDOW,
-        CW_USEDEFAULT, CW_USEDEFAULT, 1000, 1000,
+        CW_USEDEFAULT, CW_USEDEFAULT, 1500, 1000,
         nullptr, nullptr, hInstance, nullptr
     );
 }
